326f37c5ac52693b0adf3ff4a8731e4094215652
Matthew Ewing <m_r_ewing@hotmail.com>
2016-08-26 11:38:10 +0100

    Initial Commit

---
+++ RogersFirstApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RogersFirstApp
+//
+//  Created by User on 26/08/2016.
+//  Copyright © 2016 RogersDesigns. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
